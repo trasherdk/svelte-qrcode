523e391ac0dfeadb029ad6771f161917e73e748d
trasherdk <trasherdk@gmail.com>
2025-01-18 12:11:30 +0100

    Migrate to Svelte 5 systax

--- src/lib/qrcode/qrcode.svelte
+++ src/lib/qrcode/qrcode.svelte
@@ -1,23 +1,43 @@
+<script module lang="ts">
+  export interface Props {
+    content?: string;
+    size?: string;
+    padding?: string;
+    color?: string;
+    bgcolor?: string;
+    responsive?: string;
+    errorCorrection?: string;
+  }
+</script>
+
 <script lang="ts">
-import { QRCode } from "./qrcode";
-import type { Options } from "./qrcode";
+  import { QRCode } from "./qrcode";
+  import type { Options } from "./qrcode";
 
-export let content = '';
+  let {
+    content = "",
+    size = "150",
+    padding = "4",
+    color = "#000000",
+    bgcolor = "#ffffff",
+    responsive = "false",
+    errorCorrection = "M",
+  } = $props();
 
-export let size = '150';
-export let padding = '4';
-export let color = '#000000';
-export let bgcolor = '#ffffff';
-export let responsive = 'false';
-export let errorCorrection = 'M';
+  let opt: Options = {
+    content: content,
+    padding: parseInt(padding),
+    width: parseInt(size),
+    height: parseInt(size),
+    typeNumber: 4,
+    color: color,
+    background: bgcolor,
+    ecl: errorCorrection,
+    container: responsive.toLowerCase() == "true" ? "svg-viewbox" : "svg",
+    join: true,
+  };
 
-let opt : Options = { content: content,
-    padding: parseInt(padding), width: parseInt(size), height: parseInt(size), typeNumber: 4, color: color, 
-    background: bgcolor, ecl: errorCorrection, container: responsive.toLowerCase() == 'true'?'svg-viewbox':'svg',
-    join: true };
-
-let qrCode = new QRCode(opt);
-
+  let qrCode = new QRCode(opt);
 </script>
 
 {@html qrCode.svg()}
